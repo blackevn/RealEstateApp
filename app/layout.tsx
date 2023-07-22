3bbf2674eff58704ae76750fe7c89cb785f04059
blackevn <kevinlouie55@gmail.com>
2023-07-22 02:40:49 +0000

    Properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,10 @@ export default function RootLayout({ children  } : {
     return <>
             <html lang="en">
             <body>
+              <ToasterWrap/> 
             <div 
              className="">
              <SessionProvider>
-             <ToasterWrap/> 
                 {children}   
             </SessionProvider>
             </div>       
