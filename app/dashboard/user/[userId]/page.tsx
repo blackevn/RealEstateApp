693844258052f7812991a6a7ba2bc6bd2232bfdd
blackevn <kevinlouie55@gmail.com>
2023-07-27 11:45:22 +0000

    Property details

--- app/dashboard/user/[userId]/page.tsx
+++ app/dashboard/user/[userId]/page.tsx
@@ -1,9 +1,19 @@
+import { useCurrentUser } from "@/app/hooks";
 import React from "react";
 
 const UserDetails = () => {
-  return <div>
-                User Details
-        </div>;
+
+        const { data: user } = useCurrentUser()
+
+  return <div className="grid grid-cols-12">
+                <div className="grid rounded-xl col-span-5 h-full bg-gray-1 p-4">
+                        <h1>Profile</h1>
+
+                </div>
+                <div className="h-full rounded-xl grid col-span-7 bg-gray-1 p-4">
+                        <h1>Favorites</h1>
+                </div>
+        </div>
 };
 
 export default UserDetails;
