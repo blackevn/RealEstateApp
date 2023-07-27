69b875213b48d7c257aac9d17a88c4dc1dce53af
blackevn <kevinlouie55@gmail.com>
2023-07-27 21:15:32 +0000

    Property details

--- app/dashboard/user/[userId]/page.tsx
+++ app/dashboard/user/[userId]/page.tsx
@@ -10,7 +10,7 @@ const UserDetails = () => {
         const { data: user } = useCurrentUser()
         const [ editUser, handleEditUser, setEditUser ] = useToggle()
 
-  return <div> 
+  return <div className="space-y-4"> 
         <div className="flex items-center w-full justify-between">
         <h1 className="text-2xl">{user?.name}</h1>
         <Modal
@@ -26,8 +26,8 @@ const UserDetails = () => {
                <h1>Edit user profile</h1> 
         </Modal>
         </div>
-          <div className="grid grid-cols-12 gap-4">
-                <div className="grid rounded-xl col-span-5 h-full bg-gray-1 p-4">
+        <div className="lg:grid grid-cols-12 gap-4 space-y-4">
+                <div className="grid rounded-xl col-span-5 h-full bg-gray-2 p-4">
                         <h1>Profile</h1>
                         <div className="grid place-items-center gap-4">
                         <Avatar width="w-24 h-24"/>
@@ -35,9 +35,12 @@ const UserDetails = () => {
                         </div>
 
                 </div>
-                <div className="h-full rounded-xl grid col-span-7 bg-gray-1 p-4">
+                <div className="h-full rounded-xl grid col-span-7 bg-gray-2 p-4">
                         <h1>Favorites</h1>
                 </div>
+        </div>
+        <div className='w-full rounded-xl '>
+         <h1>Properties</h1>
         </div>
         </div>
 };
