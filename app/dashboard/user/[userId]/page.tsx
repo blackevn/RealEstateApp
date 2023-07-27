e9d16ef08e105c5db4b412f595e2860843978bb2
blackevn <kevinlouie55@gmail.com>
2023-07-27 12:08:37 +0000

    Property details

--- app/dashboard/user/[userId]/page.tsx
+++ app/dashboard/user/[userId]/page.tsx
@@ -7,9 +7,11 @@ const UserDetails = () => {
 
         const { data: user } = useCurrentUser()
 
-  return <div className="grid grid-cols-12">
+  return <div className="grid grid-cols-12 gap-4">
+                <h1 className="text-2xl">{user?.name}</h1>
                 <div className="grid rounded-xl col-span-5 h-full bg-gray-1 p-4">
                         <h1>Profile</h1>
+                        <h1>{user?.email}</h1>
 
                 </div>
                 <div className="h-full rounded-xl grid col-span-7 bg-gray-1 p-4">
